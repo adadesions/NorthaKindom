160765b07dc11f9a4e2780783faa55980ae158ca
adadesions <adadesions@gmail.com>
2023-06-29 10:35:02 +0700

    AI movable to goal

--- Assets/Games/SSRUGame/AgentController.cs
+++ Assets/Games/SSRUGame/AgentController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AgentController : MonoBehaviour
 {
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _walkSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,15 @@ public class AgentController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Movements();
+    }
+
+    private void Movements()
+    {
+        transform.LookAt(_target);
+        transform.position =
+            Vector3.MoveTowards(transform.position, _target.position, _walkSpeed * Time.fixedDeltaTime);
     }
 }
