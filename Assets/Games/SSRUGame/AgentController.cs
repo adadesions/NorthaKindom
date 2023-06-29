4cee4338460daff05a7f3bb9c0052a94b19ac5fe
adadesions <adadesions@gmail.com>
2023-06-29 11:14:41 +0700

    Need to fix

--- Assets/Games/SSRUGame/AgentController.cs
+++ Assets/Games/SSRUGame/AgentController.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AgentController : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    private Transform _target;
+    [SerializeField] private List<Transform> _paths;
     [SerializeField] private float _walkSpeed = 1.0f;
+    private int _curWayPoint = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_paths.Count > 0)
+        {
+            _target = _paths[0];
+        }
+         
     }
 
     // Update is called once per frame
@@ -24,5 +31,19 @@ public class AgentController : MonoBehaviour
         transform.LookAt(_target);
         transform.position =
             Vector3.MoveTowards(transform.position, _target.position, _walkSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("WayPoint"))
+        {
+            print("_curWayPoint: " + _curWayPoint.ToString());
+            _curWayPoint++;
+            if (_curWayPoint >= _paths.Count)
+            {
+                _curWayPoint = 0;
+            }
+            _target = _paths[_curWayPoint];
+        }
     }
 }
